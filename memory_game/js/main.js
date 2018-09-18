6b28b9dacfe8c54b799476bbdd7e06481d3dfa14
Joseph Lee <josephlee@Josephs-MacBook-Pro.local>
2018-09-18 06:17:28 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -31,14 +31,24 @@ if (cardsInPlay [0] === cardsInPlay [1]){
 			alert("Sorry, try again");
 	}
 }
-var flipCard = function(cardId){
-	console.log("User flipped over " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2){
 		checkForMatch();
 }
 }
-flipCard(0);
-flipCard(2);
+
+var createBoard = function(){
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
+
